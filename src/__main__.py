807e3acc3594b0301f7092e5e26cfbfffdafcd9e
Shivendu Kumar <shiv.end.you@gmail.com>
2026-01-06 17:16:30 +0530

    Added: 'help' cli command for more info

--- src/__main__.py
+++ src/__main__.py
@@ -19,5 +19,14 @@ elif cmd == "monthly":
     from src.cli.monthly import main
     main()
 
+elif cmd == "help":
+    print(f"""
+          Available commands
+          - help        : use it to list all the commands     : python -m src help
+          - walkforward : run this initially, creates the csv : python -m src walkforward
+          - query       : queries the anomalies for date      : python -m src query --date YYYY-MM-DD
+          - monthly     : queries market status for the month : python -m src monthly --month YYYY-MM  
+    """)
+
 else:
     raise ValueError(f"Unknown command: {cmd}")
